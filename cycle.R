d77cfd54133859ac0ebd7de1fa8ea212abd84b35
Harry Woo <mmmshyal@knou.ac.kr>
2020-06-10 16:43:58 +0900

    0610

--- cycle.R
+++ cycle.R
@@ -8,7 +8,7 @@ library(forecast)
 
 rm(list = ls())
 
-## 데이터 읽기 ##
+# Data Loading
 
 gdp  <- read.csv("gdpq.csv", header = TRUE)
 gdp_sa <- ts(gdp[,1]/1000, start = 1982, frequency = 4)
@@ -21,33 +21,36 @@ plot(gdp_sa, ylab = "GDP", xlab = "Time", col = "steelblue")
 
 # GDP 변동요인 분해 (계절요인은 원 자료에서 이미 조정됨)
 
-# 추세변동 추출1 : 로그변환한 계절조정계열에 HP 필터를 적용
+# 추세변동요인 계열 추출 1 : 로그변환한 계절조정계열에 호드릭-프레스콧(HP) 필터를 적용
 lgdp_sa <- log(gdp_sa)
-lgdp_hp <- mFilter(lgdp_sa, filter = "HP")
+lgdp_hp <- mFilter(lgdp_sa, filter = "HP", lambda = 1600)
 
-# 추세변동 추출2 : 지수함수를 이용하여 GDP 추세계열로 전환
+# 추세변동요인 계열 추출 2 : 지수함수를 이용하여 GDP 추세계열로 전환
 gdp_trend <- exp(lgdp_hp$trend)
 
-# 계절조정 vs. 추세계열
+# 계절조정계열 vs. 추세변동요인 계열
 forecast::autoplot(gdp_sa, series = "계절조정", lwd = 1.2) +
   autolayer(gdp_trend, series = "추세계열", lwd = 0.8, lty = 1) +
   xlab("Time") + ylab("GDP") + ggtitle(label = "실질 국내총생산 (분기별, 계절조정 vs 추세계열)") +
   scale_colour_manual(values = c("계절조정" = "steelblue", "추세계열" = "red")) +
   theme(plot.title = element_text(size = 18, hjust = 0.5, vjust = 1.5, face = "bold"))
 
-# 이동평균(3분기)을 통해 불규칙 변동을 제거, 지수함수로 재변환
+# 3분기 중심화 이동평균을 통해 불규칙변동요인을 제거, 지수함수로 재변환
 gdp_sam <- exp((lgdp_sa + lag(lgdp_sa, -1)+ lag(lgdp_sa, 1))/3)
 
+# 순환변동요인을 로그차분을 고려해 계산하면 아래와 같음
+# lgdp_sam <- (lgdp_sa + lag(lgdp_sa, -1)+ lag(lgdp_sa, 1))/3 #추세순환
+# lgdp_trend <- lgdp_hp$trend #추세
+# lgdp_cycle <- lgdp_sam - lgdp_trend
+# exp(lgdp_cycle) * 100 == gdp_cycle
+
+# 100을 기준으로 순환변동요인 계산 (100 이상 호황, 이하 불황)
+gdp_cycle <- gdp_sam / gdp_trend * 100
 gdp_irregular <- gdp_sa / gdp_sam * 100
 
-# 100을 기준으로 순환변동치 계산 (100 이상 호황, 이하 불황)
-gdp_cycle = gdp_sam / gdp_trend * 100
-
-# 순환변동
+# 순환변동요인 계열 그래프로 표현
 autoplot(gdp_cycle, lwd = 1, col = "steelblue") +
-  geom_hline(yintercept = 100, linetype = "dashed", colour = "red")
-
+  geom_hline(yintercept = 100, linetype = "dashed", colour = "black")
 
 plot(gdp_cycle, main="순환변동요인", col="steelblue", ylab="", xlab="")
   abline(h = 100, col = "red", lty = 2)
-  
